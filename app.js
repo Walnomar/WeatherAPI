67c713137e3b9fb533a8ca3e98818401e859358b
Walnomar <78608171+Walnomar@users.noreply.github.com>
2021-08-18 14:44:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.cityName')
+var desc = document.querySelector('.desc')
+var temp = document.querySelector('.temp')
+var locationIcon = document.querySelector('.icon')
+
+button.addEventListener('click', function(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&units=metric&appid=5ec0e056f3582bc36735bf5902f3d099`)
+    .then(response => response.json())
+    //.then(data => console.log(data))
+    .then(data => {
+        var nameValue = data['weather']['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+        var icon = data['weather'][0]['icon'];
+        
+        
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+        locationIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png"/>`
+    })
+    
+    .catch(err => alert("City doesn't exist"))
+})
+
