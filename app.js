15e490c1dd40576552dc507aeda883caf155a659
Walnomar <78608171+Walnomar@users.noreply.github.com>
2021-09-27 14:20:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var locationIcon = document.querySelector('.icon')
 button.addEventListener('click', function(){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&units=metric&appid=5ec0e056f3582bc36735bf5902f3d099`)
     .then(response => response.json())
-    //.then(data => console.log(data))
     .then(data => {
         var nameValue = data['weather']['name'];
         var tempValue = data['main']['temp'];
